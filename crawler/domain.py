fee1e2f590dda43dfc626743282e121688586ab9
Daegonny <igorchagasm@hotmail.com>
2020-09-10 10:54:42 -0300

    Atividade 2 concluída

--- crawler/domain.py
+++ crawler/domain.py
@@ -16,10 +16,10 @@ class Domain():
 		return timedelta(seconds=self.int_time_limit_seconds) < self.time_since_last_access
 
 	def __hash__(self):
-		return None
+		return hash(self.nam_domain)
 
 	def __eq__(self, domain):
-		return None
+		return (self.__class__ == domain.__class__ and self.nam_domain == domain.nam_domain) or self.nam_domain == domain
 
 	def __str__(self):
 		return self.nam_domain
