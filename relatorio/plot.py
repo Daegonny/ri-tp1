131012e894457e10d7f96c267939ddaa54c06935
Lucas Corrêa <lucashuati@hotmail.com>
2020-10-04 10:01:45 -0300

    Revove not used files

--- relatorio/plot.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-
-
-class Plot:
-    def plot(self, x_values, y_values, subtitle):
-        fig, ax = plt.subplots()
-        ax.plot(x_values, y_values)
-        fig.suptitle(subtitle)
