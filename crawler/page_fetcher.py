fb6a50c769d20478b31e0ce22b020d905c6d746f
Lucas Nascimento Huati Corrêa <lucashuati@hotmail.com>
2020-10-05 01:05:14 -0300

    Add velocity analisys on report (#2)
    * Add velocity analisys on report
    
    * Revove not used files

--- crawler/page_fetcher.py
+++ crawler/page_fetcher.py
@@ -1,7 +1,9 @@
-from bs4 import BeautifulSoup
 from threading import Thread
+from urllib.parse import urljoin, urlparse
+
 import requests
-from urllib.parse import urlparse,urljoin
+from bs4 import BeautifulSoup
+
 
 class PageFetcher(Thread):
     def __init__(self, obj_scheduler):
@@ -9,24 +11,24 @@ class PageFetcher(Thread):
         self.finished = False
         Thread.__init__(self)
 
-
-    def request_url(self,obj_url):
+    def request_url(self, obj_url):
         """
             Faz a requisição e retorna o conteúdo em binário da URL passada como parametro
 
             obj_url: Instancia da classe ParseResult com a URL a ser requisitada.
         """
-        response = requests.get(obj_url.geturl(),headers = {'user-agent': self.obj_scheduler.str_usr_agent})
+        response = requests.get(obj_url.geturl(), headers={
+                                'user-agent': self.obj_scheduler.str_usr_agent})
         if 'text/html' in response.headers['content-type']:
             return response.content
         else:
-            return None        
+            return None
 
-    def discover_links(self,obj_url,int_depth,bin_str_content):
+    def discover_links(self, obj_url, int_depth, bin_str_content):
         """
         Retorna os links do conteúdo bin_str_content da página já requisitada obj_url
         """
-        soup = BeautifulSoup(bin_str_content,features="lxml")
+        soup = BeautifulSoup(bin_str_content, features="lxml")
         for link in soup.select('a'):
             try:
                 obj_new_url = urlparse(urljoin(obj_url.geturl(), link['href']))
@@ -34,7 +36,7 @@ class PageFetcher(Thread):
                     int_new_depth = int_depth + 1
                 else:
                     int_new_depth = 0
-                yield obj_new_url,int_new_depth
+                yield obj_new_url, int_new_depth
             except:
                 pass
 
@@ -43,41 +45,39 @@ class PageFetcher(Thread):
             Coleta uma nova URL, obtendo-a do escalonador
         """
         url, depth = self.obj_scheduler.get_next_url()
-        
+
         if(url == None):
             self.finished = True
             return
-        
+
         response = self.request_url(url)
         if(url != None and not self.obj_scheduler.has_finished_crawl()):
-            
+
             if not self.has_no_index(response):
                 self.collect(url)
-            
+
             if not self.has_no_follow(response):
                 self.gather_links(url, depth, response)
-            
 
     def collect(self, obj_url):
         self.obj_scheduler.collect_url(obj_url)
         self.obj_scheduler.count_fetched_page()
-        print(obj_url.geturl())
-    
+
     def gather_links(self, obj_url, depth, response_content):
         links = self.discover_links(obj_url, depth, response_content)
         for (new_url, new_depth) in links:
             self.obj_scheduler.add_new_page(new_url, new_depth)
-                
+
     def has_no_index(self, response_content):
         return self.check_meta_content(response_content, 'noindex')
-    
+
     def has_no_follow(self, response_content):
         return self.check_meta_content(response_content, 'nofollow')
-                
+
     def check_meta_content(self, response_content, keyword):
         flag = False
-        soup = BeautifulSoup(response_content,features="lxml")
-        for meta in soup.find_all('meta', attrs={'name':'robots'}):
+        soup = BeautifulSoup(response_content, features="lxml")
+        for meta in soup.find_all('meta', attrs={'name': 'robots'}):
             try:
                 if (keyword in meta['content']):
                     flag = True
--- crawler/scheduler.py
+++ crawler/scheduler.py
@@ -1,15 +1,18 @@
-from urllib import robotparser
-from util.threads import synchronized
-from collections import OrderedDict
-from .domain import Domain
-from datetime import datetime, timedelta
 import time
+from collections import OrderedDict
+from datetime import datetime, timedelta
+from urllib import robotparser
+
+from util.threads import synchronized
+
+from .domain import Domain
+
 
 class Scheduler():
-    #tempo (em segundos) entre as requisições
+    # tempo (em segundos) entre as requisições
     TIME_LIMIT_BETWEEN_REQUESTS = 20
 
-    def __init__(self,str_usr_agent,int_page_limit,int_depth_limit,arr_urls_seeds):
+    def __init__(self, str_usr_agent, int_page_limit, int_depth_limit, arr_urls_seeds):
         """
             Inicializa o escalonador. Atributos:
                 - `str_usr_agent`: Nome do `User agent`. Usualmente, é o nome do navegador, em nosso caso,  será o nome do coletor (usualmente, terminado em `bot`)
@@ -30,8 +33,9 @@ class Scheduler():
         self.dic_robots_per_domain = {}
         self.finished = False
         self.list_collected_urls = []
+        self.crawl_duration = None
 
-        [self.add_new_page(url,0) for url in arr_urls_seeds]
+        [self.add_new_page(url, 0) for url in arr_urls_seeds]
 
     @synchronized
     def count_fetched_page(self):
@@ -46,14 +50,14 @@ class Scheduler():
         """
         if(self.int_page_count >= self.int_page_limit):
             if(not self.finished):
-                print("Finished with " + str(datetime.now() - self.start_time))
+                self.crawl_duration = datetime.now() - self.start_time
+                print(f"Finished with {str(self.crawl_duration)}")
             self.finished = True
             return True
         return False
 
-
     @synchronized
-    def can_add_page(self,obj_url,int_depth):
+    def can_add_page(self, obj_url, int_depth):
         """
             Retorna verdadeiro caso  profundade for menor que a maxima
             e a url não foi descoberta ainda
@@ -61,26 +65,26 @@ class Scheduler():
         return int_depth < self.int_depth_limit and (not obj_url in self.set_discovered_urls)
 
     @synchronized
-    def add_new_page(self,obj_url,int_depth):
+    def add_new_page(self, obj_url, int_depth):
         """
             Adiciona uma nova página
             obj_url: Objeto da classe ParseResult com a URL a ser adicionada
             int_depth: Profundidade na qual foi coletada essa URL
         """
-        #https://docs.python.org/3/library/urllib.parse.html
+        # https://docs.python.org/3/library/urllib.parse.html
         if(self.can_add_page(obj_url, int_depth)):
             domain = Domain(obj_url.netloc, self.TIME_LIMIT_BETWEEN_REQUESTS)
-            
+
             if not domain.nam_domain in self.dic_url_per_domain:
                 self.dic_url_per_domain[domain] = []
-            
-            self.dic_url_per_domain[domain.nam_domain].append((obj_url, int_depth))
-        
+
+            self.dic_url_per_domain[domain.nam_domain].append(
+                (obj_url, int_depth))
+
             self.set_discovered_urls.add(obj_url)
             return True
-        
-        return False
 
+        return False
 
     @synchronized
     def get_next_url(self):
@@ -88,13 +92,13 @@ class Scheduler():
         Obtem uma nova URL por meio da fila. Essa URL é removida da fila.
         Logo após, caso o servidor não tenha mais URLs, o mesmo também é removido.
         """
-        url = depth = None 
+        url = depth = None
         domains_to_remove = []
         min_time_to_wait = None
-        
+
         while(url == None and depth == None and len(self.dic_url_per_domain) > 0):
-            
-            for domain, urls in  self.dic_url_per_domain.items():
+
+            for domain, urls in self.dic_url_per_domain.items():
                 if domain.is_accessible():
                     domain.accessed_now()
                     if len(urls) > 0:
@@ -103,18 +107,19 @@ class Scheduler():
                         break
                     else:
                         domains_to_remove.append(domain.nam_domain)
-                        
+
                 elif(min_time_to_wait == None or min_time_to_wait > domain.time_will_be_acessible):
                     min_time_to_wait = domain.time_will_be_acessible
 
             if(url == None and depth == None):
-                time_to_wait = max((domain.time_will_be_acessible - datetime.now()).total_seconds(), 0)
-                time.sleep(time_to_wait)   
-        
+                time_to_wait = max(
+                    (min_time_to_wait - datetime.now()).total_seconds(), 0)
+                time.sleep(time_to_wait)
+
             for domain in domains_to_remove:
                 self.dic_url_per_domain.pop(domain)
-            
-        return  url, depth
+
+        return url, depth
 
     def get_robots(self, nam_domain):
 
@@ -125,7 +130,7 @@ class Scheduler():
         return robot_parser
 
     @synchronized
-    def can_fetch_page(self,obj_url):
+    def can_fetch_page(self, obj_url):
         """
         Verifica, por meio do robots.txt se uma determinada URL pode ser coletada
         """
@@ -133,9 +138,9 @@ class Scheduler():
         domain = obj_url.netloc
         if domain not in self.dic_robots_per_domain:
             robots = self.get_robots(domain)
-            self.dic_robots_per_domain[domain] = robots  
- 
-        return self.dic_robots_per_domain.get(domain).can_fetch(self.str_usr_agent,url)
+            self.dic_robots_per_domain[domain] = robots
+
+        return self.dic_robots_per_domain.get(domain).can_fetch(self.str_usr_agent, url)
 
     @synchronized
     def collect_url(self, obj_url):
